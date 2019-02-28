1a3d6334044a40c9f86195ac9756f18d3aecc437
m.zielezny <m.zielezny@ocado.com>
2019-02-28 18:18:52 +0100

    wip

---
+++ client/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './views/css/styles.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+
+// Views
+import { App, ErrorDisplay } from './views/Components/index';
+import { Home, CurrentWeather } from './views/Containers/index';
+
+ReactDOM.render(
+    <Router>
+      <App>
+        <Route exact path='/' component={Home}/>
+      </App>
+    </Router>
+  , document.getElementById('root'));
+
+// registerServiceWorker(); // eslint-disable-line no-undef
---
+++ client/src/views/components/index.js
@@ -0,0 +1,4 @@
+
+import { default as App } from './App';
+
+export { App};
