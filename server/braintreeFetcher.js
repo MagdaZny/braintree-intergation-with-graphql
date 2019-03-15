5c559eef10bf7353294e60a723f0e5146ad38e22
m.zielezny <m.zielezny@ocado.com>
2019-03-16 00:14:48 +0100

    Refactor braintree fetcher

--- server/braintreeFetcher.js
+++ server/braintreeFetcher.js
@@ -5,6 +5,7 @@ const ourHeaders = new Headers();
 ourHeaders.append('Content-Type', 'application/json');
 ourHeaders.append('Braintree-Version', '2019-02-23');
 ourHeaders.append('Authorization', 'Basic YjhxazNuNXQ1ank1Mno2eDo0Y2EyZDU4OThlMTFlZmI1NjhhOTY5ZDBmZTg1YWUxOA')
+const URL = 'https://payments.sandbox.braintree-api.com/graphql';
 
 exports.hello = function hello(res) {
     fetch('https://jsonplaceholder.typicode.com/todos/1')
@@ -14,8 +15,9 @@ exports.hello = function hello(res) {
         });
 }
 
+let URL = 'https://payments.sandbox.braintree-api.com/graphql';
 exports.heartbeat = function heartbeat(res) {
-    fetch('https://payments.sandbox.braintree-api.com/graphql', {
+    fetch(URL, {
         method: 'POST',
         headers: ourHeaders,
         body: JSON.stringify({ query: "query { ping }" }),
@@ -27,38 +29,45 @@ exports.heartbeat = function heartbeat(res) {
 }
 
 exports.getToken = function getToken(res) {
-    fetch('https://payments.sandbox.braintree-api.com/graphql', {
+    fetch(URL, {
         method: 'POST',
         headers: ourHeaders,
         body: JSON.stringify({ query: "mutation { createClientToken { clientToken }}" }),
     }).then(res => res.json())
         .then(data => {
-            res.send({ token : data.data.createClientToken.clientToken });
-        });  
+            res.send({ token: data.data.createClientToken.clientToken });
+        });
 }
+let chargeQuery = "mutation ExampleCharge($input: ChargePaymentMethodInput!) {" +
+    "chargePaymentMethod(input: $input) { " +
+    "transaction { " +
+    "id " +
+    "status " +
+    "}" +
+    "}" +
+    "}"
+
+// save query, call it, extract queries to a separate file
+// use some hardcoded data, db, to show how we can have operations on various psps
+
 
 exports.charge = function charge(res) {
-    fetch('https://payments.sandbox.braintree-api.com/graphql', {
+    fetch(URL, {
         method: 'POST',
         headers: ourHeaders,
-        body: JSON.stringify({ query:  
-           " mutation ExampleCharge($input: ChargePaymentMethodInput!) { \
-              chargePaymentMethod(input: $input) { \
-                transaction { \
-                  id \
-                  status \
-                }\
-              }\
-            }" , variables:
-            { "input": { 
-                  "paymentMethodId": "fake-valid-nonce",
-                  "transaction": {
-                    "amount": "11.23"
-                  }
+        body: JSON.stringify({
+            query: chargeQuery, variables:
+            {
+                input: {
+                    paymentMethodId: "fake-valid-nonce",
+                    transaction: {
+                        amount: "11.23"
+                    }
                 }
-              } }),
+            }
+        }),
     }).then(res => res.json())
         .then(data => {
             res.send({ data });
-        });  
+        });
 }
